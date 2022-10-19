289198b79be15feba45a4a0752629aa2984bb476
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-10-19 03:42:49 -0300

    camada model concluida

--- src/models/sales.models.js
+++ src/models/sales.models.js
@@ -1,25 +1,48 @@
 const snakeize = require('snakeize');
 const connection = require('./connection');
 
+const registerSalesProduct = async (sale, saleId) => {
+  const snakedSale = snakeize(sale);
+  const columns = Object.keys(snakedSale)
+    .map((column) => `${column}`)
+    .join(', ');
+
+  const placeholders = Object.keys(sale)
+    .map((_column) => '?')
+    .join(', ');
+
+  const salesProductsResponse = await connection.execute(
+    `INSERT INTO StoreManager.sales_products (sale_id, ${columns}) VALUE (?, ${placeholders})`,
+    [saleId, ...Object.values(sale)],
+  );
+  console.log(salesProductsResponse);
+};
+
 const registerSale = async (sale) => {
   const salesResponse = await connection.execute(
     'INSERT INTO StoreManager.sales(date) VALUE(NOW())',
   );
+
+  const saleId = salesResponse[0].insertId;
   // console.log(salesResponse)
-  const columns = Object.keys(sale[0])
-    .map((column) => snakeize(column))
-    .join(', ');
-  
-  const placeholders = Object.keys(sale[0])
-    .map((_column) => '?')
-    .join(', ');
-  
-  const salesProductsResponse = await connection.execute(
-    `INSERT INTO StoreManager.sales_products (${columns}) VALUE (${placeholders})`,
-  );
+  sale.forEach((singleSale) => registerSalesProduct(singleSale, saleId));
+  return saleId;
 };
 
-// registerSale();
+// A registerSale recebe o array de vendas e cadastra todas as informações
+// pertinentes no banco de dados (duas tabelas) e ainda por cima retorna
+// o id da venda
+
+registerSale([
+  {
+    productId: 1,
+    quantity: 1,
+  },
+  {
+    productId: 2,
+    quantity: 5,
+  },
+]);
 
 module.exports = {
   registerSale,
